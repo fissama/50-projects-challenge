f7681493e4f7844e0ca4df5b1e2cc3cbb39dce4e
fissama <centaurging99@gmail.com>
2021-06-21 22:40:12 +0700

    Add day 10 dad jokes

---
+++ Dad Jokes/script.js
@@ -0,0 +1,18 @@
+const jokeEl = document.getElementById("joke");
+const jokeBtn = document.getElementById("jokeBtn");
+
+const generateJoke = () => {
+  const config = {
+    headers: {
+      Accept: "application/json",
+    },
+  };
+
+  fetch("https://icanhazdadjoke.com", config)
+  .then((res) => res.json())
+  .then(data=>{
+      jokeEl.innerHTML = data.joke;
+  });
+};
+
+jokeBtn.addEventListener('click',generateJoke);
